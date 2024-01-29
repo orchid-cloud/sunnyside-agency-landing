26303310199b434531bf845bc4ff90330040e994
NZ <n.prokopyk@gmail.com>
2024-01-29 11:34:30 +0100

    hamburger menu mobile

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import './scss/style.scss';
+
+const body = document.querySelector('body');
+const btnToggle = document.querySelector('#btnToggle');
+const navContent = document.querySelector('.nav__content');
+
+function toggleMobileMenu() {
+  if (navContent.style.display === '') {
+    navContent.style.display = 'flex';
+    btnToggle.setAttribute('aria-expanded', 'true');
+    body.classList.add('noscroll');
+  } else {
+    navContent.style.display = '';
+    btnToggle.setAttribute('aria-expanded', 'false');
+  }
+}
+btnToggle.addEventListener('click', toggleMobileMenu);
